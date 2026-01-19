8f42566dd31fba850623c7b926cc79622ce64295
MehulHaritra <148323594+MehulHaritra@users.noreply.github.com>
2026-01-19 17:14:27 +0530

    Changes in netbanking

--- tests/payment/netBanking.spec.js
+++ tests/payment/netBanking.spec.js
@@ -30,7 +30,7 @@ test.describe('NetBanking Payment Functional Test', () => {
     await registerPage.enterName(testData.name);
     // await registerPage.enterEmail(testData.email);
     await registerPage.enterEmail(testData.email);
-    // await registerPage.enterMobile(testData.mobile); 
+    await registerPage.enterMobile(testData.mobile); 
      await registerPage.enterJourneydate(testData.JourneyDate);
     // await registerPage.selectSourceCountry(testData.countries.source);
     await registerPage.selectDestinationCountry(testData.countries.destination);
